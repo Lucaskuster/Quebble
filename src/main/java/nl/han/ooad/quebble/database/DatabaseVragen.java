c6a9032fa949f5aec1ce31c199c6b24610d6ca3d
Lucaskuster <62052214+lucaskuster@users.noreply.github.com>
2021-01-27 10:19:29 +0100

    Met Olav gekeken voor pull request.

--- src/main/java/nl/han/ooad/quebble/database/DatabaseVragen.java
+++ src/main/java/nl/han/ooad/quebble/database/DatabaseVragen.java
@@ -8,25 +8,25 @@ import java.util.ArrayList;
 public class DatabaseVragen {
 
     public void DatabaseVragen() {
-        Vraag vraag1 = new Vraag(1, "Wat is de hoofdstad van Peru?", );
-        Vraag vraag2 = new Vraag(2,;
+//        Vraag vraag1 = new Vraag(1, "Wat is de hoofdstad van Peru?", );
+//        Vraag vraag2 = new Vraag(2,);
     }
 
-    public ArrayList<Integer> getQuizzesId() {
-        ArrayList<Integer> ids = new ArrayList<>();
-        for (Quiz quiz : quizzes) {
-            ids.add(quiz.getQuizId());
-        }
-        return ids;
-    }
-
-    public Quiz getQuiz(int quizId) {
-        for (Quiz quiz : quizzes) {
-            if (quiz.getQuizId() == quizId) {
-                return quiz;
-            }
-        }
-        return null;
-    }
+//    public ArrayList<Integer> getQuizzesId() {
+//        ArrayList<Integer> ids = new ArrayList<>();
+//        for (Quiz quiz : quizzes) {
+//            ids.add(quiz.getQuizId());
+//        }
+//        return ids;
+//    }
+//
+//    public Quiz getQuiz(int quizId) {
+//        for (Quiz quiz : quizzes) {
+//            if (quiz.getQuizId() == quizId) {
+//                return quiz;
+//            }
+//        }
+//        return null;
+//    }
 
 }
